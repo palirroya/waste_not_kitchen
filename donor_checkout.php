58376e3174eb455d881416c0ee8f5bfa97652a69
Aaron Maryniewski <aa526825@ucf.edu>
2025-11-29 19:29:30 -0500

    extract credit card details to reusuable component

--- donor_checkout.php
+++ donor_checkout.php
@@ -101,28 +101,7 @@ if (isset($_POST["cancel"])) {
 
     <?php require_once("list_reserved_purchasable_plates.php"); ?>
 
-    <div class="card mb-4">
-        <div class="card-header bg-secondary text-white">Saved Credit Card</div>
-        <div class="card-body">
-            <?php
-            $stmt = $db_conn->prepare("
-                SELECT card_number, card_expiry
-                FROM CreditCards
-                WHERE user_id = ?
-                LIMIT 1
-            ");
-            $stmt->bind_param("i", $user_id);
-            $stmt->execute();
-            $card = $stmt->get_result()->fetch_assoc();
-            if (!$card) { ?>
-                <p>You do not have a saved credit card on file.</p>
-            <?php } else { ?>
-                <p>You will use the following saved credit card on file to complete this purchase:</p>
-                <p>Card ending in <strong><?php echo substr($card["card_number"], -4) ?></strong>
-                with expiry <strong><?php echo $card["card_expiry"] ?></strong></p>
-            <?php } ?>
-        </div>
-    </div>
+    <?php require_once("list_credit_card_details.php"); ?>
 
     <form method="POST" class="d-flex gap-2">
         <button name="confirm" class="btn btn-success <?php if ($actions_disabled) echo "disabled" ?>">Confirm Purchase</button>
